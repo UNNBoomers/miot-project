a9ed64dc20a9bf464bba841ae17444b3ccf4d282
MatyasVondra <matyas.vondra@omio.com>
2024-01-25 21:16:19 +0100

    Return unique zones

--- server/src/services/zones.service.ts
+++ server/src/services/zones.service.ts
@@ -3,10 +3,24 @@ import { ZoneDto } from "../models/zone.model";
 
 export async function getZonesService(): Promise<ZoneDto[]> {
   const csvRows = await getDesksFromCsv();
-  return csvRows.map((row) => {
-    return {
-      id: row.zoneId,
-      name: row.zoneName,
-    };
-  });
+
+  const uniqueZoneIds = new Set<string>();
+
+  return csvRows
+    .filter((row) => {
+      // Check if the zoneId is not in the Set
+      if (!uniqueZoneIds.has(row.zoneId)) {
+        // If not present, add it to the Set and return true to include it in the result
+        uniqueZoneIds.add(row.zoneId);
+        return true;
+      }
+      // If already present, return false to exclude it from the result
+      return false;
+    })
+    .map((row) => {
+      return {
+        id: row.zoneId,
+        name: row.zoneName,
+      };
+    });
 }

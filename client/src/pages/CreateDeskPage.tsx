f256a27eed6c51b2c09b78aec1fc064bcd56f5ce
JayJehlicka <jindrich.jehlicka@mibcon.cz>
2024-01-28 09:00:16 +0100

    fix desk input

--- client/src/pages/CreateDeskPage.tsx
+++ client/src/pages/CreateDeskPage.tsx
@@ -151,7 +151,7 @@ export const CreateDeskPage = ({ apiKey, onSuccessfulCreation }: CreateDeskPageP
           <input
             id='deskName'
             type='text'
-            value={zone.zoneName}
+            value={zone.deskName}
             onChange={(e) => setZone((old) => ({ ...old, deskName: e.target.value }))}
             className='mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
           />
